348dde2cccc3234763b8893ee5de30a4d7e9efa4
Alen Siljak <alen.siljak@gmx.com>
2019-05-11 17:52:19 +0200

    preparing the ui for loading balance sheet

--- views/Settings.vue
+++ views/Settings.vue
@@ -1,5 +1,28 @@
 <template>
   <div>
     <h1>Settings</h1>
+    <div>
+      <p>Load an initial balance sheet, providing the accounts list, balance amounts,
+        and commodities/currencies</p>
+      <button type="button" class="btn btn-secondary" v-on:click="loadBalanceSheet">
+        Load Balance Sheet</button>
+    </div>
   </div>
 </template>
+<script>
+export default {
+data: function() {
+    return {
+      test: null,
+    };
+  },  
+  methods: {
+    loadBalanceSheet() {
+      console.log('test')
+    },
+    test2() {
+      console.log('2')
+    }
+  }
+}
+</script>
