0662a7a9070640a01e075e154051d4acc063768a
Alen Siljak <alen.siljak@gmx.com>
2019-05-11 21:00:44 +0200

    code for selecting and checking the input files

--- views/Settings.vue
+++ views/Settings.vue
@@ -1,26 +1,46 @@
 <template>
   <div>
     <h1>Settings</h1>
+    <p>Here you can import the data required for the operation.</p>
+
     <div>
+      <h2>Balances</h2>
       <p>
-        Load an initial balance sheet, providing the accounts list, balance amounts,
-        and commodities/currencies
+        Loading a balance sheet will provide the accounts list, their balances,
+        and commodities/currencies. Export from Ledger with "ledger balances".
       </p>
-      <button
-        type="button"
-        class="btn btn-secondary"
-        v-on:click="loadBalanceSheet"
-      >Load Balance Sheet</button>
+      <input
+        type="file"
+        class="form-control-input"
+        v-on:dragover="onFileHover"
+        v-on:change="onBalancesFile"
+      >
+      <div class="form-row">
+        <textarea v-model="balanceSheetContent"></textarea>
+      </div>
     </div>
+
+    <h2>Payees</h2>
 
     <div class="mt-3">
       <p>Load payees from Ledger. Export with "ledger payees".</p>
+      <!-- <div id="drop_zone" v-on:dragover="onFileHover">Drop files here</div> -->
       <div class="form-row">
-        <div class="col">
-          <input id="payeesFile" class="form-control-file" type="file">
-        </div>
-        <button type="button" class="btn btn-secondary" v-on:click="onLoadPayees">Load Payees</button>
+        <input
+          id="payeesFile"
+          class="form-control-file"
+          type="file"
+          v-on:dragover="onFileHover"
+          v-on:change="onPayeesFile"
+        >
       </div>
+      <div class="form-row">
+        <textarea v-model="payeesContent"></textarea>
+      </div>
+    </div>
+
+    <div class="text-center mt-5 mb-5">
+      <button type="button" class="btn btn-primary" v-on:click="onOk">OK</button>
     </div>
   </div>
 </template>
@@ -28,22 +48,61 @@
 export default {
   data: function() {
     return {
-      test: null
+      balanceSheetContent: null,
+      payeesContent: null
     };
   },
   methods: {
     loadBalanceSheet() {
       console.log("test");
     },
-    test2() {
-      console.log("2");
+    onPayeesFile: function(event) {
+      this.readFileContent(event, "payeesContent");
     },
-    onLoadPayees() {
-      // todo enable uploading payee list
-      fetch('payeesFile')
-        .then(r => r.text())
-        .then(t => console.log(t))
+    onBalancesFile: function(event) {
+      this.readFileContent(event, "balanceSheetContent");
+    },
+    onFileHover: function(evt) {
+      evt.stopPropagation();
+      evt.preventDefault();
+      evt.dataTransfer.dropEffect = "copy"; // Explicitly show this is a copy.
+    },
+    onOk: function() {
+      // todo: process the file(s)
+    },
+    /**
+     * event = fileInput.changed event
+     * dataField = name of the field on data object to which the content will be saved.
+     */
+    readFileContent: function(event, dataField) {
+      // Receive a fileInput.changed event. Read the file content and
+      // invoke the callback function, if any, passing the contents.
+      // https://developer.mozilla.org/en-US/docs/Web/API/FileReader/onload
+
+      var files = event.target.files;
+      var file = files[0];
+
+      var reader = new FileReader();
+      //reader.readAsDataURL(file) => useful when adding the source to images directly.
+      var component = this;
+      reader.onload = function(event) {
+        // File was successfully read.
+        // target = FileReader
+        var content = event.target.result;
+        //console.log(content)
+
+        if (dataField) {
+          component[dataField] = content;
+        }
+      };
+      reader.readAsText(file);
     }
   }
 };
 </script>
+<style>
+textarea {
+  width: 100%;
+  height: 10em;
+}
+</style>
