0c09fbeaa939e3e305992e9406f7213b5bd0233b
Alen Siljak <alen.siljak@gmx.com>
2019-05-12 22:26:44 +0200

    placing files following the vue-cli organization

--- src/router.js
+++ src/router.js
@@ -2,9 +2,7 @@
   App router.
 */
 import Vue from 'vue'
-import VueRouter from 'vue-router'
-
-Vue.use(VueRouter)
+import Router from 'vue-router'
 
 import About from './views/About.vue'
 import Home from './views/Home.vue'
@@ -16,9 +14,15 @@ import Register from './views/Register.vue'
 import Settings from './views/Settings.vue'
 import Transaction from './views/Transaction.vue'
 
+Vue.use(Router)
+
 const routes = [
-  { path: '/', component: Home },
-  { path: '/about', component: About },
+  { path: '/', name: 'home', component: Home },
+  // route level code-splitting
+  // this generates a separate chunk (about.[hash].js) for this route
+  // which is lazy-loaded when the route is visited.
+  // component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+  { path: '/about', name: 'about', component: About },
   { path: '/bal', component: BalanceSheet },
   //{ path: '/payee/:name', component: PayeeList, props: true },
   { path: '/payee', component: PayeeList },
@@ -27,8 +31,9 @@ const routes = [
   { path: '/tx', component: Transaction }
 ]
 
-const router = new VueRouter({
+const router = new Router({
   mode: 'history',
+  base: process.env.BASE_URL,
   routes: routes,
   hashbang: false
 });
