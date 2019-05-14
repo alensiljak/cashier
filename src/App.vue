3ee5dd9a23359b3dafed8bb140d1d9d08f37ea0e
Alen Siljak <alen.siljak@gmx.com>
2019-05-14 12:01:02 +0200

    moving the main content into the panel with sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,25 @@
 <template>
   <div id="app">
-    <!-- <VueSlideout :toggleSelectors="['.toggle-button']"> -->
-      <SlideoutMenu/>
-    <!-- </VueSlideout> -->
-
-    <nav class="navbar navbar-dark bg-dark" role="navigation" aria-label="main navigation">
-      <div class="navbar-brand">
-        <img
-          src="/img/icons/favicon-32x32.png"
-          width="30"
-          height="30"
-          class="d-inline-block align-top"
-          alt
-        >
-        <router-link to="/">Cashier</router-link>
-      </div>
-
-      <router-link to="/tx">New Transaction</router-link>
-      <router-link to="/bal">Balance Sheet</router-link>
-      <router-link to="/reg">Register</router-link>
-      <router-link to="/payees">Payees</router-link>
-      <router-link to="/settings">Settings</router-link>
-      <router-link to="/about">About</router-link>
+    <!-- This is the sidebar -->
+    <nav id="menu">
+      <header>
+        <h2>Menu</h2>
+      </header>
     </nav>
-
-    <button type="button" v-on:click="testSidebar">Sidebar</button>
-
-    <router-view></router-view>
+    <!-- this is the content area -->
+    <main id="panel">
+      <MainPanel/>
+    </main>
   </div>
 </template>
 <script>
-//import VueSlideout from "vue-slideout"
-import SlideoutMenu from "./components/SlideoutMenu"
-import Slideout from 'slideout'
+import MainPanel from "./views/MainPanel";
+import Slideout from "slideout";
 
 export default {
   name: "Cashier",
   mounted() {
-    //console.log('from app:', this.$store)
-    // slide menu
-    //var slideout = new
-    //console.log(this.$children[0].slideout.isOpen());
-
+    // Show the slideout menu.
     var slideout = new Slideout({
       panel: document.getElementById("panel"),
       menu: document.getElementById("menu"),
@@ -50,15 +28,9 @@ export default {
     });
   },
   methods: {
-    testSidebar() {
-      //var opened = this.$children[0].slideout.opened
-      console.log("alive");
-      //this.$children[0].slideout.toggle()
-    }
   },
   components: {
-    // VueSlideout,
-    SlideoutMenu
+    MainPanel
   }
 };
 </script>
---
+++ src/views/MainPanel.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <nav class="navbar navbar-dark bg-dark" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <img
+          src="/img/icons/favicon-32x32.png"
+          width="30"
+          height="30"
+          class="d-inline-block align-top"
+          alt
+        >
+        <router-link to="/">Cashier</router-link>
+      </div>
+
+      <router-link to="/tx">New Transaction</router-link>
+      <router-link to="/bal">Balance Sheet</router-link>
+      <router-link to="/reg">Register</router-link>
+      <router-link to="/payees">Payees</router-link>
+      <router-link to="/settings">Settings</router-link>
+      <router-link to="/about">About</router-link>
+    </nav>
+
+    <router-view></router-view>
+  </div>
+</template>
