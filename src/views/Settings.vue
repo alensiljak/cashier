d879604496c18cdb8f147cad5bedf8c639e5a15a
Alen Siljak <alen.siljak@gmx.com>
2019-05-14 10:13:37 +0200

    correcting the import statement

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -54,7 +54,7 @@
 </template>
 <script>
 import PayeeImporter from "../components/PayeeImporter";
-import SET_PAYEES from "../mutations";
+import { SET_PAYEES } from "../mutations";
 
 export default {
   data: function() {
