f899ce1920c8cf2b19722c8cccd4f59dd6da54dc
Alen Siljak <alen.siljak@gmx.com>
2019-05-15 11:29:56 +0200

    localforage demo

--- src/views/DataTest.vue
+++ src/views/DataTest.vue
@@ -1,25 +1,55 @@
 <template>
-    <div>
-        <h1>Data test</h1>
+  <div>
+    <ActionBar/>
 
-        <div class="row">
-            <textarea v-bind="dummy">Some dummy text</textarea>
-            <button type="button" v-on:click=saveData>Save some data</button>
-        </div>
+    <div class="container-fluid">
+      <h1>Data test</h1>
+
+      <p>
+        Existing data:
+        <span>{{ existingData }}</span>
+      </p>
+      <div>
+        <textarea v-model="dummy"></textarea>
+        <button type="button" class="btn btn-secondary" v-on:click="saveData">Save some data</button>
+      </div>
     </div>
+  </div>
 </template>
 <script>
+import ActionBar from '../components/ActionBar.vue';
+import localforage from 'localforage';
+
 export default {
-    data: function() {
-        return {
-            something: 'nothing',
-            dummy: null
+  data: function() {
+    return {
+      something: "nothing",
+      dummy: null,
+      existingData: null
+    };
+  },
+  created() {
+    this.dummy = "Some dummy text";
+
+    // load data from storage
+    localforage.getItem("key").then(value => {
+        if (value == null) {
+            value = "<no data found>"
         }
+      this.existingData = value;
+    });
+  },
+  methods: {
+    saveData() {
+      // console.log("saving", this.dummy);
+      localforage.setItem("key", this.dummy).then(this.doSomethingElse);
     },
-    methods: {
-        saveData() {
-            console.log(this.dummy)
-        }
+    doSomethingElse() {
+      console.log("should have saved:", this.dummy);
     }
-}
+  },
+  components: {
+    ActionBar
+  }
+};
 </script>
