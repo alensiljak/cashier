5088da8bfe7adf4af649dc1297cfa704bcc33896
Alen Siljak <alen.siljak@gmx.com>
2019-05-16 11:26:55 +0200

    styling about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,52 +2,60 @@
   <div>
     <ActionBar title="About"/>
 
-    <div class="row">
+    <div class="container">
       <div style="width:100%;" class="text-center">
         <h1>About Cashier</h1>
         <img src="/img/icons/icon96.png">
-        <p>
-          This software is intended to be used as a part of Plain-Text Accounting ecosystem.
-          More specifically, it is a GUI helper for
-          <a
-            href="https://ledger-cli.org"
-          >Ledger</a>.
-        </p>
-        <p>
-          The source code is available in a
-          <a
-            href="https://gitlab.com/alensiljak/cashier"
-          >source code repository</a> on GitLab.
-        </p>
       </div>
+      <p>
+        This software is intended to be used as a part of Plain-Text Accounting ecosystem.
+        More specifically, it is a GUI helper for
+      </p>
+      <ul>
+        <li>
+          <a href="https://ledger-cli.org">Ledger</a>, and
+        </li>
+        <li>
+          <a href="https://hledger.org">Hledger</a>
+        </li>
+      </ul>
+      <p>
+        The source code is available in a
+        <a
+          href="https://gitlab.com/alensiljak/cashier"
+        >source code repository</a> on GitLab.
+      </p>
+
+      <h2>Credits</h2>
+      <ul>
+        <li>
+          <a
+            href="https://www.freepik.com/free-photos-vectors/sale"
+          >Sale vector created by macrovector - www.freepik.com</a>
+        </li>
+        <li>
+          <a
+            href="https://www.freepik.com/free-photos-vectors/gold"
+          >Gold vector created by freepik - www.freepik.com</a>
+        </li>
+      </ul>
+
+      <h2>Components</h2>
+      <ul>
+        <li>
+          <a href="https://getbootstrap.com/">Bootstrap 4</a>
+        </li>
+        <li>
+          <a href="https://vuejs.org/">Vue JS, Vuex, Vue-cli</a>
+        </li>
+        <li>
+          <a href="https://github.com/mango/slideout">Slideout.js</a>
+        </li>
+        <li>
+          <a href="https://github.com/charliekassel/vuejs-datepicker">vue-datepicker</a>
+        </li>
+      </ul>
     </div>
-
-    <h2>Credits</h2>
-    <ul>
-      <li>
-        <a
-          href="https://www.freepik.com/free-photos-vectors/sale"
-        >Sale vector created by macrovector - www.freepik.com</a>
-      </li>
-      <li>
-        <a
-          href="https://www.freepik.com/free-photos-vectors/gold"
-        >Gold vector created by freepik - www.freepik.com</a>
-      </li>
-    </ul>
-
-    <h2>Components</h2>
-    <ul>
-      <li>
-        <a href="https://getbootstrap.com/">Bootstrap 4</a>
-      </li>
-      <li>
-        <a href="https://vuejs.org/">Vue JS, Vuex, Vue-cli</a>
-      </li>
-      <li>
-        <a href="https://github.com/mango/slideout">Slideout.js</a>
-      </li>
-    </ul>
   </div>
 </template>
 <script>
