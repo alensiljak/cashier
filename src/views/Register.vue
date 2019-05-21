cd52523974c74034782100876dc3242bbdd30f65
Alen Siljak <alen.siljak@gmx.com>
2019-05-21 10:31:03 +0200

    menu for export option

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,5 +1,31 @@
 <template>
   <q-page padding class="bg-colour1 text-colour2">
+    <!-- Toolbar -->
+        <q-header elevated class="glossy">
+      <q-toolbar class="text-colour2">
+        <q-btn flat dense round @click="menuClicked" aria-label="Menu" icon="menu"/>
+
+        <q-toolbar-title>Register</q-toolbar-title>
+
+        <q-btn flat round dense icon="more_vert">
+          <q-menu>
+            <q-list dark style="min-width: 175px" class="bg-colour1">
+              <!-- dense -->
+              <!-- <q-item clickable v-close-popup>
+                <q-item-section>Synchronize</q-item-section>
+                <q-item-section side>
+                  <q-icon name="sync"/>
+                </q-item-section>
+              </q-item> -->
+              <q-item clickable v-close-popup>
+                <q-item-section @click="exportRegister">Export</q-item-section>
+              </q-item>
+            </q-list>
+          </q-menu>
+        </q-btn>
+      </q-toolbar>
+    </q-header>
+
     <div>Transactions go here. Show all if not filtered by a specific account.</div>
 
     <q-list dark>
@@ -32,9 +58,7 @@ export default {
   },
 
   created() {
-    this.$store.commit(SET_TITLE, "Register");
-    this.$store.commit(MAIN_TOOLBAR, true)
-
+    this.$store.commit(MAIN_TOOLBAR, false);
 
     this.loadData();
   },
@@ -43,6 +67,10 @@ export default {
   },
 
   methods: {
+    exportRegister() {
+      // todo export the register from here
+      console.log('export register command')
+    },
     loadData() {
       // load all transactions and related postings
       appService
