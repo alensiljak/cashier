32dae8757d03235dd3b3bdfb3b712b1fe503bb36
Alen Šiljak <alen.siljak@pm.me>
2019-11-28 11:45:07 +0100

    issuing warnings for console

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
   ],
   rules: {
     //'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
   parserOptions: {
