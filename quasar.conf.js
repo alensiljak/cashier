f23b9745b7b2cf1439f5db5d88a204c3bc26a728
Alen Šiljak <alen@alensiljak.eu.org>
2020-11-06 20:23:04 +0100

    styles clean-up

--- quasar.conf.js
+++ quasar.conf.js
@@ -21,7 +21,7 @@ module.exports = function (/* ctx */) {
     boot: ["fontAwesome", "version"],
 
     // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-css
-    css: ["app.scss"],
+    //css: ["app.scss"],
 
     // https://github.com/quasarframework/quasar/tree/dev/extras
     extras: [
