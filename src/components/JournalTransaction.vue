1d27f8fea5bdafa619aeeba6e9e8286807ec709a
Alen Šiljak <alen@alensiljak.eu.org>
2020-11-08 22:15:46 +0100

    aligning and darkening comments in journal

--- src/components/JournalTransaction.vue
+++ src/components/JournalTransaction.vue
@@ -2,7 +2,7 @@
   <q-item clickable class="text-amber-2 q-px-none">
     <q-item-section @click="itemClicked(tx.id)">
       <q-item-label>{{ tx.date }} {{ tx.payee }}</q-item-label>
-      <q-item-label v-if="tx.note" caption class="q-ml-xl text-amber-2">{{ tx.note }}</q-item-label>
+      <q-item-label v-if="tx.note" caption class="q-ml-lg text-colour2">{{ tx.note }}</q-item-label>
       <q-item-label caption class="q-ml-lg text-amber-2">
         <div v-for="posting in tx.postings" :key="posting.id" class="row">
           <div class="col">{{ posting.account }}</div>
