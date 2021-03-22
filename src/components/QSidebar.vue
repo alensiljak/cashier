2bdca838e5f4de4a687c2432aeb18be414ff5b4f
Alen Šiljak <code@alensiljak.eu.org>
2021-03-22 22:30:59 +0100

    fixing the logo path for Open Store

--- src/components/QSidebar.vue
+++ src/components/QSidebar.vue
@@ -283,7 +283,7 @@
     <q-img class="absolute-top bg-colour5" style="height: 150px">
       <div class="absolute-bottom bg-transparent text-colour2 text-center">
         <q-avatar size="56px" class="q-mb-sm" @click="onHeaderClick">
-          <img src="/icons/icon64.png">
+          <img src="../../public/icons/icon64.png">
         </q-avatar>
         <!-- <div class="text-weight-bold">Cashier</div> -->
         <!-- <div>@cashier</div> -->
