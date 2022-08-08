0c5a52aac0515d755de19d97a84416c55003489f
Alen Šiljak <as@alensiljak.eu.org>
2022-08-08 22:35:48 +0200

    use a different port than cashiersync

--- quasar.conf.js
+++ quasar.conf.js
@@ -78,7 +78,7 @@ module.exports = function (/* ctx */) {
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-devServer
     devServer: {
       https: false,
-      port: 8080,
+      port: 8081,
       //open: true // opens browser window automatically
       open: false
     },
