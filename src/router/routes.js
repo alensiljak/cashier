c068f618988ae7ce58e2220b89d357e959b37140
Alen Šiljak <dev@alensiljak.eu.org>
2022-08-17 21:10:05 +0200

    removing the Calculator component due to the size

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,224 +3,224 @@
 */
 
 // components
-const CalculatorNeu = () => import("../pages/Calculator.vue");
+//const CalculatorNeu = () => import("../pages/Calculator.vue");
 
 const routes = [
   {
-    path: "/",
-    component: () => import("../layouts/Qlayout.vue"),
+    path: '/',
+    component: () => import('../layouts/Qlayout.vue'),
     children: [
-      { path: "", name: "root", redirect: "/home" },
+      { path: '', name: 'root', redirect: '/home' },
       {
-        path: "/about",
-        name: "about",
-        component: () => import("../pages/About.vue"),
+        path: '/about',
+        name: 'about',
+        component: () => import('../pages/About.vue'),
       },
       {
-        path: "/account/:id",
-        name: "account",
+        path: '/account/:id',
+        name: 'account',
         component: () =>
-          import(/* webpackChunkName: 'account' */ "../pages/Account.vue"),
+          import(/* webpackChunkName: 'account' */ '../pages/Account.vue'),
       },
       {
-        path: "/accounts",
-        name: "accounts",
-        component: () => import("../pages/Accounts.vue"),
+        path: '/accounts',
+        name: 'accounts',
+        component: () => import('../pages/Accounts.vue'),
       },
       {
-        path: "/assetallocation",
-        name: "assetallocation",
+        path: '/assetallocation',
+        name: 'assetallocation',
         component: () =>
           import(
-            /* webpackChunkName: 'assetallocation' */ "../pages/AssetAllocation.vue"
+            /* webpackChunkName: 'assetallocation' */ '../pages/AssetAllocation.vue'
           ),
       },
       {
-        path: "/assetallocationhelp",
-        name: "assetallocationhelp",
+        path: '/assetallocationhelp',
+        name: 'assetallocationhelp',
         component: () =>
           import(
-            /* webpackChunkName: 'assetallocation' */ "../pages/AssetAllocationHelp.vue"
+            /* webpackChunkName: 'assetallocation' */ '../pages/AssetAllocationHelp.vue'
           ),
       },
       {
-        path: "/assetallocationsetuphelp",
-        name: "assetallocationsetuphelp",
+        path: '/assetallocationsetuphelp',
+        name: 'assetallocationsetuphelp',
         component: () =>
           import(
-            /* webpackChunkName: 'assetallocation' */ "../pages/AASetupHelp.vue"
+            /* webpackChunkName: 'assetallocation' */ '../pages/AASetupHelp.vue'
           ),
       },
       {
-        path: "/assetclassdetail/:fullname?",
-        name: "assetclassdetail",
+        path: '/assetclassdetail/:fullname?',
+        name: 'assetclassdetail',
         component: () =>
           import(
-            /* webpackChunkName: 'assetallocation' */ "../pages/AssetClassDetail.vue"
+            /* webpackChunkName: 'assetallocation' */ '../pages/AssetClassDetail.vue'
           ),
       },
       {
-        path: "/bal",
-        name: "balanceSheet",
+        path: '/bal',
+        name: 'balanceSheet',
         component: () =>
-          import(/* webpackChunkName: 'bal' */ "../pages/QBalanceSheet.vue"),
+          import(/* webpackChunkName: 'bal' */ '../pages/QBalanceSheet.vue'),
       },
       {
-        path: "/cache",
-        name: "cache",
+        path: '/cache',
+        name: 'cache',
         component: () =>
-          import(/* webpackChunkName: 'cache' */ "../pages/Cache.vue"),
+          import(/* webpackChunkName: 'cache' */ '../pages/Cache.vue'),
       },
+      // {
+      //   path: "/calculator",
+      //   components: {
+      //     default: CalculatorNeu,
+      //   },
+      // },
       {
-        path: "/calculator",
-        components: {
-          default: CalculatorNeu,
-        },
-      },
-      {
-        path: "/calendar",
-        name: "calendar",
+        path: '/calendar',
+        name: 'calendar',
         component: () =>
-          import(/* webpackChunkName: 'calendar' */ "../pages/Calendar.vue"),
+          import(/* webpackChunkName: 'calendar' */ '../pages/Calendar.vue'),
       },
       {
-        path: "/commodities",
-        name: "commodities",
+        path: '/commodities',
+        name: 'commodities',
         component: () =>
           import(
-            /* webpackChunkName: 'commodities' */ "../pages/Commodities.vue"
+            /* webpackChunkName: 'commodities' */ '../pages/Commodities.vue'
           ),
       },
       {
-        path: "/export/:type?",
-        name: "export",
-        component: () => import("../pages/Export.vue"),
+        path: '/export/:type?',
+        name: 'export',
+        component: () => import('../pages/Export.vue'),
       },
       {
-        path: "/favourites",
-        name: "favourites",
-        component: () => import("../pages/Favourites.vue"),
+        path: '/favourites',
+        name: 'favourites',
+        component: () => import('../pages/Favourites.vue'),
       },
       {
-        path: "/favouritesreorder",
-        name: "favreorder",
-        component: () => import("../pages/FavouritesReorder.vue"),
+        path: '/favouritesreorder',
+        name: 'favreorder',
+        component: () => import('../pages/FavouritesReorder.vue'),
       },
       {
-        path: "/help",
-        name: "help",
+        path: '/help',
+        name: 'help',
         component: () =>
-          import(/* webpackChunkName: 'help' */ "../pages/Help.vue"),
+          import(/* webpackChunkName: 'help' */ '../pages/Help.vue'),
       },
       {
-        path: "/home",
-        name: "home",
-        component: () => import("../pages/Home.vue"),
+        path: '/home',
+        name: 'home',
+        component: () => import('../pages/Home.vue'),
       },
       {
-        path: "/journal",
-        name: "journal",
-        component: () => import("../pages/Journal.vue"),
+        path: '/journal',
+        name: 'journal',
+        component: () => import('../pages/Journal.vue'),
       },
       {
-        path: "/lots/:symbol",
-        name: "lots",
+        path: '/lots/:symbol',
+        name: 'lots',
         component: () =>
-          import(/* webpackChunkName: 'lots' */ "../pages/Lots.vue"),
+          import(/* webpackChunkName: 'lots' */ '../pages/Lots.vue'),
       },
       {
-        path: "/payees",
-        name: "payees",
-        component: () => import("../pages/Payees.vue"),
+        path: '/payees',
+        name: 'payees',
+        component: () => import('../pages/Payees.vue'),
       },
       {
-        path: "/prices",
-        name: "prices",
+        path: '/prices',
+        name: 'prices',
         component: () =>
-          import(/* webpackChunkName: 'prices' */ "../pages/Prices.vue"),
+          import(/* webpackChunkName: 'prices' */ '../pages/Prices.vue'),
       },
       {
-        path: "/reg/:name",
-        name: "register",
-        component: () => import("../pages/Register.vue"),
+        path: '/reg/:name',
+        name: 'register',
+        component: () => import('../pages/Register.vue'),
       },
       {
-        path: "/reorder-postings",
-        name: "reorder postings",
+        path: '/reorder-postings',
+        name: 'reorder postings',
         component: () =>
           import(
-            /* webpackChunkName: 'reorder-postings' */ "../pages/ReorderPostings.vue"
+            /* webpackChunkName: 'reorder-postings' */ '../pages/ReorderPostings.vue'
           ),
       },
       {
-        path: "/restore/:type?",
-        name: "restore",
+        path: '/restore/:type?',
+        name: 'restore',
         component: () =>
-          import(/* webpackChunkName: 'restore' */ "../pages/Restore.vue"),
+          import(/* webpackChunkName: 'restore' */ '../pages/Restore.vue'),
       },
       {
-        path: "/settings",
-        name: "settings",
-        component: () => import("../pages/Settings.vue"),
+        path: '/settings',
+        name: 'settings',
+        component: () => import('../pages/Settings.vue'),
       },
       {
-        path: "/scheduledtransactions",
-        name: "scheduledtransactions",
+        path: '/scheduledtransactions',
+        name: 'scheduledtransactions',
         component: () =>
           import(
-            /* webpackChunkName: 'scheduledtx' */ "../pages/ScheduledTxList.vue"
+            /* webpackChunkName: 'scheduledtx' */ '../pages/ScheduledTxList.vue'
           ),
       },
       {
-        path: "/scheduledtxactions/:id",
-        name: "scheduledtxactions",
+        path: '/scheduledtxactions/:id',
+        name: 'scheduledtxactions',
         props: true,
         component: () =>
           import(
-            /* webpackChunkName: 'scheduledtx' */ "../pages/ScheduledTxActions.vue"
+            /* webpackChunkName: 'scheduledtx' */ '../pages/ScheduledTxActions.vue'
           ),
       },
       {
-        path: "/scheduledtxeditor/:id?",
-        name: "scheduledtxeditor",
+        path: '/scheduledtxeditor/:id?',
+        name: 'scheduledtxeditor',
         component: () =>
           import(
-            /* webpackChunkName: 'scheduledtx' */ "../pages/ScheduledTxEditor.vue"
+            /* webpackChunkName: 'scheduledtx' */ '../pages/ScheduledTxEditor.vue'
           ),
       },
       {
-        path: "/sync",
-        name: "sync",
-        component: () => import("../pages/Sync.vue"),
+        path: '/sync',
+        name: 'sync',
+        component: () => import('../pages/Sync.vue'),
       },
       {
-        path: "/tx/:id?",
-        name: "tx",
-        component: () => import("../pages/Transaction.vue"),
+        path: '/tx/:id?',
+        name: 'tx',
+        component: () => import('../pages/Transaction.vue'),
       },
       {
-        path: "/tx-actions/:id",
-        name: "tx-actions",
+        path: '/tx-actions/:id',
+        name: 'tx-actions',
         props: true,
-        component: () => import("../pages/TransactionActions.vue"),
+        component: () => import('../pages/TransactionActions.vue'),
       },
       {
-        path: "/tx-search",
-        name: "tx-search",
+        path: '/tx-search',
+        name: 'tx-search',
         component: () =>
-          import(/* webpackChunkName: 'txsearch' */ "../pages/TxSearch.vue"),
+          import(/* webpackChunkName: 'txsearch' */ '../pages/TxSearch.vue'),
       },
       {
-        path: "/xact",
-        name: "xact",
+        path: '/xact',
+        name: 'xact',
         component: () =>
-          import(/* webpackChunkName: 'xact' */ "../pages/Xact.vue"),
+          import(/* webpackChunkName: 'xact' */ '../pages/Xact.vue'),
       },
       {
-        path: "/pcloud",
-        name: "pcloud",
+        path: '/pcloud',
+        name: 'pcloud',
         component: () =>
-          import(/* webpackChunkName: 'pcloud' */ "../pages/Pcloud.vue"),
+          import(/* webpackChunkName: 'pcloud' */ '../pages/Pcloud.vue'),
       },
     ],
   },
@@ -230,10 +230,10 @@ const routes = [
   //   redirect: '/'
   // },
   {
-    path: "/:catchAll(.*)*",
-    component: () => import("pages/Error404.vue"),
+    path: '/:catchAll(.*)*',
+    component: () => import('pages/Error404.vue'),
   },
-];
+]
 
 // Always leave this as last one
 // routes.push({
@@ -241,4 +241,4 @@ const routes = [
 //   component: () => import('pages/Error404.vue')
 // })
 
-export default routes;
+export default routes
