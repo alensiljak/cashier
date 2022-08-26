3ac4ef96fe301c90b9d089c2e6e06afd42220ef8
Alen Šiljak <dev@alensiljak.eu.org>
2022-08-26 10:31:37 +0200

    turning on the history mode

--- quasar.config.js
+++ quasar.config.js
@@ -53,7 +53,8 @@ module.exports = configure(function (/* ctx */) {
         node: 'node16',
       },
 
-      vueRouterMode: 'hash', // available values: 'hash', 'history'
+      //vueRouterMode: 'hash', // available values: 'hash', 'history'
+      vueRouterMode: 'history',
       // vueRouterBase,
       // vueDevtools,
       // vueOptionsAPI: false,
