ca90dbadf3b7e5a2ddf9910eaf6b74325f152334
Alen Šiljak <dev@alensiljak.eu.org>
2022-10-08 21:16:03 +0200

    trying indexeddb directly

--- tests/settings.spec.ts
+++ tests/settings.spec.ts
@@ -1,5 +1,4 @@
 import { test, expect } from '@playwright/test'
-import db from '../src/store/indexedDb'
 
 const testData = [
   { id: 1, name: 'test' },
@@ -20,14 +19,55 @@ test.describe('Settings', () => {
     // let win = await page.evaluate(() => window)
     // let ib = win.indexedDB
     // console.log('indexedDB enabled:', ib)
-    console.log(db.accounts)
-    await db.accounts.put({ name: 'account1' })
-    console.log(db.accounts)
 
-    await page.evaluate(() => {
-      // seed
-      console.log('yo')
+    // console.log(db.accounts)
+    // await db.accounts.put({ name: 'account1' })
+    // console.log(db.accounts)
+
+    await page.goto('http://localhost:9200/')
+
+    const result = await page.evaluate(() => {
+      if (!window.indexedDB) {
+        //window.alert("Your browser doesn't support a stable version of IndexedDB.")
+        throw new Error('no support for indexeddb')
+      }
+
+      return new Promise(function (resolve, reject) {
+        const storeName = 'cashier'
+        //let db
+        let openRequest = window.indexedDB.open(storeName)
+        openRequest.onerror = function (event) {
+          reject(Error('Error text'))
+        }
+        openRequest.onupgradeneeded = function (event) {
+          // Objectstore does not exist. Nothing to load
+          //event.target.transaction.abort();
+          reject(Error('Data store not found ("upgrade needed" received)'))
+        }
+        openRequest.onsuccess = function (event) {
+          // seed
+          let db = openRequest.result
+          const tx = db.transaction([storeName])
+          const store = tx.objectStore(storeName)
+
+          const objectRequest = store.put({ name: 'test account' })
+          objectRequest.onerror = function (event) {
+            reject(Error('Error text'))
+          }
+
+          objectRequest.onsuccess = function (event) {
+            if (objectRequest.result) {
+              resolve(objectRequest.result)
+            } else {
+              reject(Error('object not found'))
+            }
+          }
+
+          //return db
+        }
+      })
     })
+    console.log(result)
   })
 
   test.beforeEach(async ({ page }) => {
