b008e58c6fbbaf3f54bc58b059aa4e0c5b5fc825
Alen Šiljak <dev@alensiljak.eu.org>
2023-07-27 00:23:28 +0200

    displaying text on the page

--- src/pages/LedgerWasm.vue
+++ src/pages/LedgerWasm.vue
@@ -3,15 +3,21 @@
         <toolbar title="Ledger-rs" />
 
         <p>Testing Ledger-rs Wasm usage.</p>
+
+        <div>
+            {{ message }}
+        </div>
     </q-page>
 </template>
 <script setup lang="ts">
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
 import Toolbar from '../components/CashierToolbar.vue'
 import useNotifications from 'src/lib/CashierNotification'
 import * as ledger from 'ledger-rs-lib'
 
 const Notification = useNotifications()
+
+const message = ref('')
 
 onMounted(async () => {
     // console.log(ledger)
@@ -20,6 +26,8 @@ onMounted(async () => {
     console.log(text)
 
     Notification.info(text)
+
+    message.value = text
 })
 
 </script>
