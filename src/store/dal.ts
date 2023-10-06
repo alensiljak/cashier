6942d65efd99e057b7a1a160d7774827625d0493
Alen Šiljak <dev@alensiljak.eu.org>
2023-10-06 09:13:17 +0200

    removing postings table use on save

--- src/store/dal.ts
+++ src/store/dal.ts
@@ -61,7 +61,6 @@ class CashierDAL {
     }
 
     // convert to pocos
-    //let postings = tx.postings.map((txposting) => toRaw(txposting))
     let postings = tx.postings
     // check whether the accounts exist!
     if (postings.length) {
@@ -72,67 +71,19 @@ class CashierDAL {
         const account = posting.account
         if (!accountNames.includes(account)) {
           throw new Error(
-            `The account ${account} does not exist! Please create first.`
+            `The account ${account} does not exist! Please create first.`,
           )
         }
       })
     }
-    //tx.postings = postings
-
-    this._processPostings(tx)
 
     // save all items in a transaction
-    let id = await db.transaction(
-      'rw',
-      db.transactions,
-      db.postings,
-      async () => {
-        db.postings.bulkPut(tx.postings)
-
-        //delete tx.postings
-
-        // returns the transaction id
-        let id = await db.transactions.put(tx)
-        return id
-      }
-    )
+    let id = await db.transaction('rw', db.transactions, async () => {
+      // returns the transaction id
+      let id = await db.transactions.put(tx)
+      return id
+    })
     return id as number
-  }
-
-  /**
-   * Ensures correct data/types for new postings during saving.
-   * Removes the missing postings.
-   */
-  async _processPostings(tx: Transaction) {
-    // modifications
-    // set transaction id on postings
-    tx.postings.forEach((posting) => (posting.transactionId = tx.id as number))
-    const newPostingIds: number[] = tx.postings.map(
-      (posting) => posting.id
-    ) as number[]
-
-    // todo: Ensure only one posting with no amount (ledger requirement)?
-
-    // Delete any removed postings in the database. Get the posting ids from the existing record.
-    await this._deleteRemovedPostings(newPostingIds, tx.id)
-  }
-
-  async _deleteRemovedPostings(newPostingIds: number[], txId?: number) {
-    if (!newPostingIds) return
-    if (!txId) return
-
-    let existingTx: Transaction = await db.transactions.get(txId)
-    let postings = existingTx.postings
-    let oldPostingIds = postings.map((posting) => posting.id) as number[]
-
-    for (let i = 0; i < oldPostingIds.length; i++) {
-      let oldPostingId = oldPostingIds[i]
-
-      if (newPostingIds.indexOf(oldPostingId) < 0) {
-        // console.log('delete', oldPostingIds[i])
-        db.postings.delete(oldPostingIds[i])
-      }
-    }
   }
 }
 
